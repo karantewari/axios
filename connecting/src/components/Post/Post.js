a1d442df4013a9abea6e5396c3da29c8d43962d1
Karan <karan@karantewari.in>
2018-06-29 18:42:32 +0530

    axios implemented, just displaying fetched data

---
+++ connecting/src/components/Post/Post.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "./Post.css";
+
+const post = props => (
+  <article className="Post">
+    <h1>{props.title}</h1>
+    <h1>{props.id}</h1>
+    <div className="Info">
+      <div className="Author">Author</div>
+    </div>
+  </article>
+);
+
+export default post;
