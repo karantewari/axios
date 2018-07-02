b41087c454cdb8f1cf5aeee5010feb509b76522c
Karan <karan@karantewari.in>
2018-07-02 18:49:52 +0530

    added flow and sliced

--- connecting/src/containers/Blog/Blog.js
+++ connecting/src/containers/Blog/Blog.js
@@ -4,27 +4,46 @@ import Post from "../../components/Post/Post";
 import FullPost from "../../components/FullPost/FullPost";
 import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
-import Axios from "axios";
+import axios from "axios";
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPostId: null
   };
   componentDidMount() {
-    Axios.get("https://jsonplaceholder.typicode.com/posts").then(response => {
-      this.setState({ posts: response.data });
-      //console.log(response);
-    });
+    axios.get("https://jsonplaceholder.typicode.com/posts")
+      .then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map(post => {
+          return {
+            ...post,
+            author: "Karan"
+          }
+        })
+        this.setState({ posts: updatedPosts });
+        //console.log(response);
+      });
+  }
+  postSelectedHandler = (id) => {
+    this.setState({ selectedPostId: id })
   }
   render() {
     const posts = this.state.posts.map(post => {
-      return <Post title={post.title} key={post.id} id={post.id} />
+      return <Post
+        title={post.title}
+        key={post.id}
+        id={post.id}
+        author={post.author}
+        clicked={() => this.postSelectedHandler(post.id)} />
     });
     return (
       <div>
         <section className="Posts">{posts}</section>
         <section>
-          <FullPost />
+          <FullPost
+            id={this.state.selectedPostId}
+          />
         </section>
         <section>
           <NewPost />
